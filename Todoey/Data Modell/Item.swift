79d866ad270775ddd41cda1fc81e1561b4b65e68
Munir <munirek@munir-macbook-air.local>
2018-07-22 21:18:41 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Modell/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Items.swift
+//  Todoey
+//
+//  Created by Munir Dávid Hassan on 2018. 07. 22..
+//  Copyright © 2018. Munir Dávid Hassan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    
+    var done: Bool = false
+    
+}
